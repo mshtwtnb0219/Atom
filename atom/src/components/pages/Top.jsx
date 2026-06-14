9045d918f829ebb077165f272a6b6b683e0c7665
masa <b77yellow@gmail.com>
2026-06-14 13:57:18 +0900

    axios

--- atom/src/components/pages/Top.jsx
+++ atom/src/components/pages/Top.jsx
@@ -3,6 +3,7 @@ import { SecondaryButton } from "../atoms/button/SecondaryBurron";
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { UserContext } from "../../provider/UserProvider";
+import axios from "axios";
 export const Top = () => {
   const navgate = useNavigate();
   const { setUserInfo } = useContext(UserContext);
@@ -15,6 +16,28 @@ export const Top = () => {
     setUserInfo({ isAdmin: false });
     navgate("/users");
   };
+
+  // axiosの実行確認
+  const onClickUsers = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const onClickUser1 = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users?id=1")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <SContainer>
       <h1>TOPページです</h1>
@@ -22,6 +45,14 @@ export const Top = () => {
       <br />
       <br />
       <SecondaryButton onClick={onClickGeneral}>一般ユーザー</SecondaryButton>
+      <br />
+      <br />
+      <SecondaryButton onClick={onClickUsers}>axios検証 users</SecondaryButton>
+      <br />
+      <br />
+      <SecondaryButton onClick={onClickUser1}>
+        axios検証 id=1のuser
+      </SecondaryButton>
     </SContainer>
   );
 };
